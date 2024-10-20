660727aec8d87c8eee11ef037f82a7c876db6e33
chocydar <daryljohnebinum@gmail.com>
2024-10-20 16:10:37 +0100

    nav

---
+++ src/ui/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../assets/images/logo.svg";
+import "../../assets/styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar flex items-center justify-between bg-gray-900 p-4">
+      <div className="flex items-center">
+        <a
+          href="/"
+          className="flex items-center rounded-xl hover:bg-gray-800 font-bold text-white"
+        >
+          <img src={logo} alt="logo" className="h-8 mr-2" />
+          ideal-dollop&nbsp;&nbsp;
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
